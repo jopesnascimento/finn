c7b6dc9984f485b73f2d3f99d96b13d4d586575a
jopesnascimento <jopesouzanascimento@gmail.com>
2023-06-27 14:48:44 -0300

    finn, o humano exit close 1

---
+++ script.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('img')
+
+function troca(){
+    document.body.style.background = "black"
+    document.body.style.color = "yellow"
+    img.setAttribute('src', 'https://external-preview.redd.it/v9rmXPXlIRiDcdW7W2t7k34rD_AFvyeCxKV0y9ipCeU.jpg?auto=webp&s=7473138fae99f17301330be2f862d4e5e682b45a');
+    document.querySelector("p").textContent = "EU SOU O BATMAN!"
+    document.querySelector("h1").textContent = "OLHA JAKE!"
+}
+
+img.addEventListener('click',troca)
