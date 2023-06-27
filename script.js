66ba9248ced54d8b5812fdc27a25407dd13c5039
jopesnascimento <jopesouzanascimento@gmail.com>
2023-06-27 15:15:55 -0300

    batiman

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let img = document.querySelector('img')
+const audio = new Audio('batiman');
+
 
 function troca(){
     document.body.style.background = "black"
@@ -6,6 +8,8 @@ function troca(){
     img.setAttribute('src', 'https://external-preview.redd.it/v9rmXPXlIRiDcdW7W2t7k34rD_AFvyeCxKV0y9ipCeU.jpg?auto=webp&s=7473138fae99f17301330be2f862d4e5e682b45a');
     document.querySelector("p").textContent = "EU SOU O BATMAN!"
     document.querySelector("h1").textContent = "OLHA JAKE!"
+    audio.play()
+
 }
 
 img.addEventListener('click',troca)
